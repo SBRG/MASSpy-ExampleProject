aa5a6606d13cdb08c86a70603edb6061b5e90d75
z-haiman <zhaiman@eng.ucsd.edu>
2020-12-23 13:40:10 -0800

    Initial commit setup.py
    Initial commit __init__.py w/ __version__ attr
    
    Initial commit pyproject.toml
    
    Initial commit MANIFEST.in w/ license, README, pyrpoject.toml, and src included
    
    Initial commit setup.cfg

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""This module contains the setup for the installation of MASSpy-ExampleProject."""
+
+# All arguments are defined in `setup.cfg`.
+import setuptools
+
+
+setuptools.setup()
